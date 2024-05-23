a51bab61daf51d2d6fb0b80c2daaf675e5d4ee28
Kongji82 <Kongji4092@gmail.com>
2024-05-23 22:58:14 +0900

    [bug-fix] test api

---
+++ src/main/java/org/example/orem/domain/smaple/TestRestController.java
@@ -0,0 +1,15 @@
+package org.example.orem.domain.smaple;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestRestController {
+    @GetMapping("/test")
+    public ResponseEntity<?> pingTest() {
+        return ResponseEntity.ok(true);
+    }
+}
